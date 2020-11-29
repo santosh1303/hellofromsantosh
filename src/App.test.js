818b9e806191e11840249683eb86ff91b2ee1aca
Santosh Bukkashetti <santoshb.sdm@gmail.com>
2020-11-29 16:50:25 -0500

    Minor change
    Minor change

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Location);
+  const linkElement = screen.getByText(/location);
   expect(linkElement).toBeInTheDocument();
 });
